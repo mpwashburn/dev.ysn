e788ccbb09200f515775d6783c343439e11f1658
Michael Washburn <washburn.michael@gmail.com>
2016-11-20 13:46:20 -0800

    Updates to Front-page
    removed content-frontpage.css file and merged with style.css. Added new
    sub-section under Posts and Pages called “front page”. Removed Second
    CTA, added _s classes to front page to keep dry, removed excess divs
    and styling.

--- template-parts/content-homepage.php
+++ template-parts/content-homepage.php
@@ -1,26 +1,22 @@
-<div class="ysn-logo">
-    <img class="img-responsive" src="<?php bloginfo('template_directory'); ?>/images/YSN_Logo_2015_100x100-02.png">
-    <!-- <h1>YSN</h1> -->
-</div>
+<div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
 
-<!--<hr id="divider" style="border: 1.5px solid #4BBFAF;" class="divider">-->
-<div class="ysn-copy">
-    <p>We are a digital content platform whose mission is to illuminate the new world of opportunity for the next-generation global workforce. With our online training courses, seminars, New York Times best-selling book, and much more, we guide young people all over the world through the process of making today’s new work possibilities a reality using practical advice and best practices.</p>
-</div>
+	<div class="ysn-logo">
+            <img class=" img-responsive" src="<?php bloginfo('template_directory'); ?>/images/YSN_Logo_2015_100x100-02.png">
+        </div>
 
-<div class="cta-container">
-    <div class="cta-card cta-card-1">
-        <a href="#">
-            <h5>Exploring Your Potential</h5>
-            <h6>Debitis lucilius conclusionemque</h6> 
-        </a>
-    </div>
-            
-         
-    <div class="cta-card cta-card-2">
-        <a href="#">
-            <h5>About YSN</h5>
-            <h6>Debitis lucilius conclusionemque</h6>   
-        </a>
-    </div>
-</div>
+        <div class="ysn-copy">
+            <p>We are a digital content platform whose mission is to illuminate the new world of opportunity for the next-generation global workforce. With our online training courses, seminars, New York Times best-selling book, and much more, we guide young people all over the world through the process of making today’s new work possibilities a reality using practical advice and best practices.</p>
+        </div>
+
+        
+            <div class="cta-card">
+                <a href="#">
+                    <h5>Exploring Your Potential</h5>
+                    <h6>Debitis lucilius conclusionemque</h6> 
+                </a>
+            </div>
+      
+
+    </main><!-- #main -->
+</div><!-- #primary -->
